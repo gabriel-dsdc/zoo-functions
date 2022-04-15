6ae87b7b04f43e62897e28bd6c315a63743bed59
gabriel-dsdc <gabriel.ds.duarte99@gmail.com>
2022-04-14 23:12:15 -0300

    Requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,8 @@
 const { species } = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+const getAnimalsOlderThan = (animal, age) => {
   const foundAnimal = species.find((specie) => specie.name === animal);
   return foundAnimal.residents.every((specie) => specie.age >= age);
-}
+};
 
 module.exports = getAnimalsOlderThan;
