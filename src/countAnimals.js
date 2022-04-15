357cc77556050deac006a7e487ba5b3976400f9f
gabriel-dsdc <gabriel.ds.duarte99@gmail.com>
2022-04-15 18:46:35 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAll = () =>
+  species.reduce((newObject, animals) => {
+    const count = animals.residents.reduce((counter) => counter + 1, 0);
+    const object = newObject; // ESlint...
+    object[animals.name] = count;
+    return object;
+  }, {});
+
+const countAnimals = (animal) => {
+  if (!animal) {
+    return countAll();
+  }
+  const { specie, sex } = animal;
+  return species.find((chosenAnimal) => chosenAnimal.name === specie)
+    .residents.reduce((count, animals) => {
+      if (animals.sex === sex) {
+        return count + 1;
+      }
+      if (!sex) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+};
 
 module.exports = countAnimals;
