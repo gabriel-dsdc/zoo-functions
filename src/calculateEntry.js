19d44bd4c5f1469c0bfe84df6036493f7f0922fb
gabriel-dsdc <gabriel.ds.duarte99@gmail.com>
2022-04-15 20:10:15 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,48 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const boringLinter = (age, object) => {
+  const newObject = object;
+  if (age >= 50) {
+    newObject.senior = newObject.senior + 1 || 1;
+  } else {
+    newObject.adult = newObject.adult + 1 || 1;
+  }
+  return newObject;
+};
+
+const ageCounter = (age, object) => {
+  const newObject = object;
+  if (age >= 18) {
+    boringLinter(age, object);
+  } else {
+    newObject.child = newObject.child + 1 || 1;
+  }
+  return newObject;
+};
+
+const calcTotalPrice = (visitors) => {
+  let totalPrice = 0;
+  if (visitors.child > 0) {
+    totalPrice += prices.child * visitors.child;
+  }
+  if (visitors.adult > 0) {
+    totalPrice += prices.adult * visitors.adult;
+  }
+  if (visitors.senior > 0) {
+    totalPrice += prices.senior * visitors.senior;
+  }
+  return totalPrice;
+};
+
+const countEntrants = (entrants) =>
+  entrants.reduce((object, people) => ageCounter(people.age, object), {});
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const visitors = countEntrants(entrants);
+  return calcTotalPrice(visitors);
 }
 
 module.exports = { calculateEntry, countEntrants };
