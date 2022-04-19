da15353f80dec860e5f3d1a03554466490aff4a4
gabriel-dsdc <gabriel.ds.duarte99@gmail.com>
2022-04-18 21:50:57 -0300

    Requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,38 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const sortAnimals = (residentsName, sorted) => {
+  if (sorted) {
+    return residentsName.sort((a, b) => (a > b ? 1 : -1));
+  }
+  return residentsName;
+};
+
+const withNames = (residents, options) => {
+  const residentsName = [];
+  const { includeNames, sex, sorted } = options;
+
+  residents.forEach((resident) => {
+    if (!sex && includeNames) {
+      residentsName.push(resident.name);
+    }
+    if (sex === resident.sex && includeNames) {
+      residentsName.push(resident.name);
+    }
+  });
+  return sortAnimals(residentsName, sorted);
+};
+
+const getAnimalMap = (options) => species.reduce((newObject, specie) => {
+  const object = newObject;
+  const { name, location, residents } = specie;
+
+  if (!options || !options.includeNames) {
+    object[location].push(name);
+  } else {
+    const residentsName = withNames(residents, options);
+    object[location].push({ [name]: residentsName });
+  }
+  return object;
+}, { NE: [], NW: [], SE: [], SW: [] });
 
 module.exports = getAnimalMap;
